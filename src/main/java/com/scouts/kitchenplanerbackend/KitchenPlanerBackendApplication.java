c7479aa243fda3c2506373ca1d7845bb9df910e2
kittyahy <78612604+kittyahy@users.noreply.github.com>
2023-11-23 22:07:02 +0100

    Change module root directory

---
+++ src/main/java/com/scouts/kitchenplanerbackend/KitchenPlanerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scouts.kitchenplanerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenPlanerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenPlanerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scouts/kitchenplanerbackend/KitchenPlanerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scouts.kitchenplanerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenPlanerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
