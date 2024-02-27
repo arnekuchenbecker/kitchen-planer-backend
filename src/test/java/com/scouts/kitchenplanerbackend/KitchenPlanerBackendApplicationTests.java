22e3a44561056fdf2e71e3f9813e205ccc786dac
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-02-27 15:25:54 +0100

    some change

--- src/test/java/com/scouts/kitchenplanerbackend/KitchenPlanerBackendApplicationTests.java
+++ src/test/java/com/scouts/kitchenplanerbackend/KitchenPlanerBackendApplicationTests.java
@@ -18,7 +18,7 @@ package com.scouts.kitchenplanerbackend;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// Some change
 @SpringBootTest
 class KitchenPlanerBackendApplicationTests {
 
