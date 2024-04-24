70ccf8b77794af6a8295bfb56cc63e6f31da3b9a
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-04-24 16:24:19 +0200

    Model classes for Projects

---
+++ src/main/java/com/scouts/kitchenplanerbackend/AllergenPerson.java
@@ -0,0 +1,29 @@
+/*
+ * KitchenPlanerApp is the android app frontend for the KitchenPlaner, a tool
+ * to cooperatively plan a meal plan for a campout.
+ * Copyright (C) 2023-2024 Arne Kuchenbecker, Antonia Heiming, Anton Kadelbach, Sandra Lanz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+package com.scouts.kitchenplanerbackend;
+
+import java.util.Date;
+import java.util.List;
+
+public record AllergenPerson(String name,
+                             Date arrivalDate,
+                             Date leaveDate,
+                             String arrivalMeal,
+                             String leaveMeal,
+                             List<String> allergen,
+                             List<String> traces) {
+}
