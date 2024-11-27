6d9b2e89e5073cd8699f1df4451d8983dd1f3f78
kittyahy <78612604+kittyahy@users.noreply.github.com>
2024-11-27 14:27:35 +0100

    init a project to the database

--- src/main/java/com/scouts/kitchenplanerbackend/Preloader.java
+++ src/main/java/com/scouts/kitchenplanerbackend/Preloader.java
@@ -20,6 +20,7 @@ import com.scouts.kitchenplanerbackend.projectdtos.AllergenPerson;
 import com.scouts.kitchenplanerbackend.projectdtos.Project;
 import com.scouts.kitchenplanerbackend.services.ProjectService;
 import org.slf4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -53,5 +54,6 @@ public class Preloader {
         return args -> {
             logger.info("Insert new Project with Id: " + projectService.saveNewProject(project));
         };
+
     }
 }
